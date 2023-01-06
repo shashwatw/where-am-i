91a19acfcdb187424513065f2125f5b4137818f2
shashwatwankhedekar <shashwatwankhedekar>
2023-01-06 11:23:45 +0530

    neighnour country

--- script.js
+++ script.js
@@ -120,11 +120,21 @@ setTimeout(() => {
 
 //more simplified way
 
+
 const getCountryData = function (country) {
 
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => { renderCountry(data[0])
+        
+        const neighbour = data[0].borders[0];
+        if(!neighbour) return;
+
+        // Country 2
+        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);  
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0],'neighbour'));
 };
 // getCountryData('portugal');
 getCountryData('bharat');

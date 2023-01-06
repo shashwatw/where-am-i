3ab933798b28f96aa0b58b11727cf7b1b568fde9
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-01-06 16:01:41 +0530

    contri

--- script.js
+++ script.js
@@ -150,6 +150,7 @@ const getCountryData = function (country) {
 //test
 // getCountryData('portugal');
 // getCountryData('bharat');
+// contribution new
 
 btn.addEventListener('click', function () {
     getCountryData('bharat');
